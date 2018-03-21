59a5728af5a375e7be96e0953579e4249c5009bb
Xiao Lu <xiao.lu@ygomi.com>
2018-03-21 17:56:36 +0800

    add a case

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "eigen3/Eigen/Eigen"
+#include "Eigen/Eigen"
 
 // calculate the coordinate (x, y, z) of d in frame [a, b, c]
 void project2F(const Eigen::Vector3d &a, const Eigen::Vector3d &b, const Eigen::Vector3d &c,
@@ -18,14 +18,21 @@ void project2F(const Eigen::Vector3d &a, const Eigen::Vector3d &b, const Eigen::
 
 int main()
 {
-    std::pair<Eigen::Vector3d, Eigen::Vector3d> segmet;
-    std::tuple<Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d> triangle;
+    Eigen::Vector3d A(0, 0, 0);
+    Eigen::Vector3d B(1, 0, 0);
+    Eigen::Vector3d C(0, 1, 0);
+    Eigen::Vector3d E(1, 1, 1);
+    Eigen::Vector3d F(0, 0, -1);
+    std::pair<Eigen::Vector3d, Eigen::Vector3d> segmet(E, F);
+    std::tuple<Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d> triangle(A, B, C);
 
     Eigen::Vector3d a = std::get<0>(triangle);
     Eigen::Vector3d b = std::get<1>(triangle);
     Eigen::Vector3d c = std::get<2>(triangle);
     Eigen::Vector3d e = segmet.first;
     Eigen::Vector3d f = segmet.second;
+
+    bool isInter = true;
 
     // Step1: construct a coordinate frame whose unit vertor is ab, ac and ab x ac
     Eigen::Vector3d ab = b - a;
@@ -46,7 +53,7 @@ int main()
     // if z1 and z2 are both positive or negative, ef will not intersect with plan abc
     if (z1 * z2 > 0)
     {
-        return false;
+        isInter = false;
     }
 
     // step4: if ef intersect with plane abc, judge if it intersect with triangle abc
@@ -58,14 +65,23 @@ int main()
     // if (x, y) is not in first quadrant, it will not be in triangle abc
     if (x < 0 || y < 0)
     {
-        return false;
+        isInter = false;
     }
 
     // if (x, y) is above line x + y = 1, it is also not in triangle abc
     if (x + y > 1)
     {
-        return false;
+        isInter = false;
     }
 
-    return true;
+    if (isInter)
+    {
+        std::cout << "intersect";
+    }
+    else
+    {
+        std::cout << "not intersect";
+    }
+
+    return 0;
 }
